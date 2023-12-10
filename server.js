cf2d42d5031ddff77d976e29947c1939c6b84e33
Sumit Bajaj <sumit96bajaj@gmail.com>
2023-12-10 20:46:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const fs = require('fs');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-// process.on('uncaughtException', (err) => {
-//   console.log(err.name, err.message);
-//   console.log('Unhandled exception Shutting down');
-//   process.exit(1);
-// });
+// const fs = require('fs');
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+// // process.on('uncaughtException', (err) => {
+// //   console.log(err.name, err.message);
+// //   console.log('Unhandled exception Shutting down');
+// //   process.exit(1);
+// // });
 
-dotenv.config({ path: './config.env' });
-const DB = process.env.DATABASE;
-mongoose
-  .connect(DB, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log('Connection successful'));
-// .catch(function () {
-//   console.log('Promise Rejected');
-// });
+// dotenv.config({ path: './config.env' });
+// const DB = process.env.DATABASE;
+// mongoose
+//   .connect(DB, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then(() => console.log('Connection successful'));
+// // .catch(function () {
+// //   console.log('Promise Rejected');
+// // });
 const app = require('./app');
 
 
